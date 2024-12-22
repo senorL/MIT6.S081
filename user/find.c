d6c2b15eea60943b63df0488f157c2d094408cc4
senorL <2870510275@qq.com>
2024-12-22 17:20:16 +0800

    add regular expression in find

--- user/find.c
+++ user/find.c
@@ -4,6 +4,7 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
+int match(char*, char*);
 void find(char *path, char *name) {
     int fd;
     char buf[512];
@@ -50,7 +51,7 @@ void find(char *path, char *name) {
 
         if (st.type == T_DEVICE || st.type == T_FILE) {
             // file
-            if (strcmp(de.name, name) == 0) {
+            if (match(name, de.name)) {
                 printf("%s\n", buf);
             }
         } 
@@ -84,4 +85,37 @@ int main(int argc, char *argv[]) {
     exit(0);
     
 
+}
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int match(char *re, char *text) {
+    if (re[0] == '^')
+        return matchhere(re + 1, text);
+    do {
+        if (matchhere(re, text)) 
+            return 1;
+    } while(*text++ != '\0');
+    return 0;
+}
+
+int matchhere(char *re, char *text) {
+    if (re[0] == '\0') 
+        return 1;
+    if (re[1] == '*') 
+        return matchstar(re[0], re + 2, text);
+    if (re[0] == '$' && re[1] == '\0')
+        return *text == '\0';
+    if (*text != '\0' && (re[0] == '.' || re[0] == text[0]))
+        return matchhere(re + 1, text + 1);
+    return 0;
+}
+
+int matchstar(int c, char *re, char *text) {
+    do {
+        if (matchhere(re, text))
+            return 1;
+    } while(*text != '\0' && (*text++ == c || c == '.'));
+    return 0;
 }
